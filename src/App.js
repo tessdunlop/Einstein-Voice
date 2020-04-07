e9e753f42150bed02ef2cbb0f5390d3e97df4bb8
tessdunlop <tdunlop@tdunlop-ltm.internal.salesforce.com>
2020-04-07 14:07:28 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import React, { Component } from "react";
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+
+  
+  constructor(props) {
+    super(props);
+    
+  
+    this.state = {
+      postId: ""
+    }
+  }
+
+  componentDidMount(){
+    let params = {
+      "projectId": "our-project-123",
+      "name": "my project",
+      "labels": {
+        "mylabel": "prod"
+      }
+  }
+  const auth_key = process.env.REACT_APP_AUTH_KEY;
+
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', 'Authorization': auth_key },
+    body: JSON.stringify(params)
+};
+fetch('https://cloudresourcemanager.googleapis.com/v1/projects/', requestOptions)
+    .then(response => response.json())
+    .then(data => this.setState({ postId: data.id }));
+
+    console.log(this.state.postId);
+    console.log(auth_key);
+
+}
+  render () {
+    return (
+      <div>
+      <div>Please create a new project:</div>
+      projectId <input type="text"></input>
+      </div>
+    );
+  }
+  
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+};
+
+  
